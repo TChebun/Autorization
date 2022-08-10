2099c2408c2864699ee45d4a93ad74739b8a77ca
TChebun <t.chebun@gmail.com>
2022-08-10 15:17:46 +0200

    fix sh script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:TChebun/Autorization.git master:gh-pages
+ git push -f git@github.com:TChebun/Autorization.git main:gh-pages
 
 cd -
