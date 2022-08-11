89a152cafcdebb47556c193398b8616c143aa60a
TChebun <t.chebun@gmail.com>
2022-08-11 15:31:01 +0200

    feat(views): add registration page

--- src/views/RegistrationPage/RegistrationPage.vue
+++ src/views/RegistrationPage/RegistrationPage.vue
@@ -1,12 +1,43 @@
 <template>
   <div class="registerPage">
-    <h2 class="recoveryPage-title">Registration</h2>
+    <form-title title="Registration"/>
+    <div class="registerPage__wrapper">
+      <form-input
+          type="text"
+          name="login"
+          placeholderName="Login"
+      />
+      <form-input
+          type="text"
+          name="email"
+          placeholderName="E-mail"
+      />
+      <form-input
+          type="password"
+          name="password"
+          placeholderName="Password"
+      />
+    </div>
+    <div class="registerPage-buttons">
+      <form-button title="Join"/>
+      <form-button title="Login" @onClick="changePage" />
+    </div>
   </div>
 </template>
 
 <script>
+import FormInput from '@/components/input/FormInput.vue';
+import FormButton from '@/components/buttons/FormButton.vue';
+import FormTitle from "@/components/title/title.vue";
+
 export default {
-  name: "RegistrationPage"
+  name: "RegistrationPage",
+  components: {FormInput, FormButton, FormTitle},
+  methods: {
+    changePage: () => {
+      this.$router.push({name: '/'})
+    }
+  }
 }
 </script>
 
