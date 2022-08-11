9047625f1fab49049fe1bd5c9efd485cb6d15615
TChebun <t.chebun@gmail.com>
2022-08-11 15:33:21 +0200

    feat(components): add title at components

---
+++ src/components/title/title.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2 class="title">{{ title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormTitle',
+  props: ['title']
+}
+</script>
+
+<style scoped lang="scss" src="./title.scss"></style>
